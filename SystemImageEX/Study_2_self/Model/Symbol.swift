2264dc9d14f097d144479ef8b99d36338c319248
DONOTINTO <goods99j@gmail.com>
2023-03-03 14:51:51 +0900

    add: SystemImageEX 프로젝트 업로드

---
+++ SystemImageEX/Study_2_self/Model/Symbol.swift
@@ -0,0 +1,18 @@
+//
+//  Symbol.swift
+//  Study_2_self
+//
+//  Created by 이중엽 on 2023/03/02.
+//
+
+import UIKit
+
+class Symbol {
+    init(image: UIImage, name: String) {
+        self.image = image
+        self.name = name
+    }
+    
+    var image: UIImage
+    var name: String
+}
---
+++ SystemImageEX/Study_2_self/Model/mainTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  mainTableViewCell.swift
+//  Study_2_self
+//
+//  Created by 이중엽 on 2023/03/02.
+//
+
+import UIKit
+
+class mainTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var mainImage: UIImageView!
+}

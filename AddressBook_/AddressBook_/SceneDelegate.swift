4897ea3c7ec5a47b1a68971832e96aef85bf8c1d
DONOTINTO <goods99j@gmail.com>
2023-03-10 16:46:06 +0900

    update:: main controller 설정 오류 수정

--- AddressBook_/AddressBook_/SceneDelegate.swift
+++ AddressBook_/AddressBook_/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let mainViewController = UIViewController()
+        let mainViewController = ViewController()
         
         self.window?.rootViewController = mainViewController
         window?.makeKeyAndVisible()

f855b124a94300eb45c071014b315d42606f8c4b
DONOTINTO <goods99j@gmail.com>
2023-03-13 15:08:36 +0900

    init:: cell 클래스 추가 및 연결

---
+++ DdayCalendar/DdayCalendar/MainTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MainTableViewCell.swift
+//  DdayCalendar
+//
+//  Created by 이중엽 on 2023/03/13.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var cellImageView: UIImageView!
+    @IBOutlet weak var dDayLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var memoLabel: UILabel!
+}

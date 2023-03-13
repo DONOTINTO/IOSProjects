5df716a376ac180329c96bd6b58787f16395269f
DONOTINTO <goods99j@gmail.com>
2023-03-13 15:16:46 +0900

    init:: 메인 VC 액션 연결

--- DdayCalendar/DdayCalendar/ViewController.swift
+++ DdayCalendar/DdayCalendar/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func addButtonClicked(_ sender: Any) {
+        let detailVC = DetailViewController()
+        
+    }
+    
+    @IBAction func editButtonClicked(_ sender: Any) {
+    }
 }
 

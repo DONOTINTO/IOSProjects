936f1a31ea14cc3822166522a1e879ea56f496c7
DONOTINTO <goods99j@gmail.com>
2023-03-13 15:13:08 +0900

    init:: DetailVC 추가 및 연결

---
+++ DdayCalendar/DdayCalendar/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  DdayCalendar
+//
+//  Created by 이중엽 on 2023/03/13.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var mainDatePicker: UIDatePicker!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mainTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
